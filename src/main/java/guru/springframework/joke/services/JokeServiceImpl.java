8bb6989a4f6c5d63573684fd79747d3a147d2c53
hvtham <hvtham@gmail.com>
2019-05-04 11:00:25 +0700

    Added Java Configuration

--- src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
+++ src/main/java/guru/springframework/joke/services/JokeServiceImpl.java
@@ -1,17 +1,16 @@
 package guru.springframework.joke.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JokeServiceImpl implements JokeService{
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    @Autowired
-    public JokeServiceImpl() {
-        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = chuckNorrisQuotes;
     }
+
 
     public String getJoke(){
 
